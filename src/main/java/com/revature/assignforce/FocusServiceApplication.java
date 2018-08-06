590241b07b4c80e816cbf15f8e64a505d0836617
Ty Lowery <Lowery91@gmail.com>
2018-08-05 21:58:46 -0400

    Removed unused dependencies from pom file

--- src/main/java/com/revature/assignforce/FocusServiceApplication.java
+++ src/main/java/com/revature/assignforce/FocusServiceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrix
 @EnableRabbit
